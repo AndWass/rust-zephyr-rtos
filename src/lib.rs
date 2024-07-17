476d42d5270a32dbeec9572f4425569ad3446a7c
Andreas Wass <wass.andreas@gmail.com>
2024-07-17 19:14:20 +0200

    Initial thread support

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ pub mod bindings;
 
 pub mod print;
 pub mod sync;
+pub mod thread;
 pub mod time;
 
 pub(crate) mod macros;
@@ -37,6 +38,34 @@ pub extern "C" fn rust_thread(num: core::ffi::c_int) {
         printk!("Thread {}, releasing lock\n", num);
     }
 }
+
+extern "C" fn test_thread_main(
+    _: *mut core::ffi::c_void,
+    _: *mut core::ffi::c_void,
+    _: *mut core::ffi::c_void,
+) {
+    let name = unsafe {
+        let current = crate::bindings::k_current_get();
+        let name = crate::bindings::k_thread_name_get(current);
+        if name.is_null() {
+            "<NULL>"
+        } else {
+            core::ffi::CStr::from_ptr(name)
+                .to_str()
+                .unwrap_or("<INVALID>")
+        }
+    };
+    use crate::printk;
+    loop {
+        let mut count = COUNT.lock();
+        *count += 1;
+        printk!("Rust thread '{}', count = {}\n", name, *count);
+        unsafe { bindings::k_msleep(1000) };
+        printk!("Rust thread '{}' releasing lock\n", name);
+    }
+}
+
+thread_define!(rust_test_thread, 2048, test_thread_main);
 
 #[cfg(not(test))]
 mod not_test {
